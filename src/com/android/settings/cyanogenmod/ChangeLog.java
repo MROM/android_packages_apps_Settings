ce48cb4587f3270fafd17b5193719ef066f04fe6
mmontuori <michael.montuori@gmail.com>
2013-01-29 17:23:30 -0500

    MROM changelog

--- src/com/android/settings/cyanogenmod/ChangeLog.java
+++ src/com/android/settings/cyanogenmod/ChangeLog.java
@@ -43,12 +43,39 @@ import com.android.internal.app.AlertController;
 public class ChangeLog extends Fragment {
 
     private static final String CHANGELOG_PATH = "/system/etc/CHANGELOG-CM.txt";
+    private static final String CHANGELOG_PATH_MROM = "/system/etc/CHANGELOG-MROM.txt";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
         InputStreamReader inputReader = null;
         StringBuilder data = null;
+
+        try {
+            data = new StringBuilder(2048);
+            char tmp[] = new char[2048];
+            int numRead;
+            inputReader = new FileReader(CHANGELOG_PATH_MROM);
+            while ((numRead = inputReader.read(tmp)) >= 0) {
+                data.append(tmp, 0, numRead);
+            }
+        } catch (IOException e) {
+            data.append(this.getString(R.string.changelog_error));
+        } finally {
+            try {
+                if (inputReader != null) {
+                    inputReader.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+
+        if (TextUtils.isEmpty(data)) {
+            showErrorAndFinish();
+            return;
+        }
+
+        inputReader = null;
         try {
             data = new StringBuilder(2048);
             char tmp[] = new char[2048];
